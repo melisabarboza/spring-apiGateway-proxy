78594f8644994de3c61a89aa2a547a9fc093c427
Melisa <melisa.barboza@hotmail.com>
2022-03-07 14:16:28 -0300

    SpringCloudGateway

---
+++ src/main/java/com/cloudgateway/proxy/Application.java
@@ -0,0 +1,15 @@
+package com.cloudgateway.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
